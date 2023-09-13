8e5f5615bd7ea64686d8c0b4155ab4a107a1a1b7
Fabian <fabian.sepulveda345@gmail.com>
2023-09-12 22:14:31 -0300

    SplashActivity done

--- app/src/main/java/org/fabiansh/pantallas95/SplashActivity.kt
+++ app/src/main/java/org/fabiansh/pantallas95/SplashActivity.kt
@@ -1,11 +1,86 @@
 package org.fabiansh.pantallas95
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
+
+    val tagLog = "SplashActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // Load the fade-in animation
+        val fadeIn = AnimationUtils.loadAnimation(applicationContext, R.anim.fadein)
+
+        // Load the growth animation
+        val scaleUp = AnimationUtils.loadAnimation(applicationContext, R.anim.scaleup)
+
+        val rot = AnimationUtils.loadAnimation(applicationContext, R.anim.rot)
+
+        // Create an AnimationSet to combine both animations
+        val animationSet = AnimationSet(true)
+        animationSet.addAnimation(fadeIn)
+        animationSet.addAnimation(scaleUp)
+        animationSet.addAnimation(rot)
+
+
+        val imageView = findViewById<ImageView>(R.id.imageView2)
+
+        fadeIn.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                // Animation started
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                // Animation ended; proceed to MainActivity
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Optional: Close the splash screen activity
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                // Animation repeated
+            }
+        })
+        imageView.startAnimation(fadeIn)
+        ///
+        rot.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                // Animation started
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                // Animation ended; proceed to MainActivity
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Optional: Close the splash screen activity
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                // Animation repeated
+            }
+        })
+        imageView.startAnimation(rot)
+        ///
+        val seconds = 2000.toLong()
+        val delayMillis = seconds!!.toLong() // 2 seconds (adjust as needed)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: Close the splash screen activity
+        }, delayMillis)
     }
+
+
+
 }
