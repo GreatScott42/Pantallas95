d9b6c8cf48d50973b75a483b0908e4ce0a781f23
Fabian <fabian.sepulveda345@gmail.com>
2023-09-16 01:20:05 -0300

    menu added in main activity
    added preferences activity

--- app/src/main/java/org/fabiansh/pantallas95/MainActivity.kt
+++ app/src/main/java/org/fabiansh/pantallas95/MainActivity.kt
@@ -1,11 +1,35 @@
 package org.fabiansh.pantallas95
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.preferencias -> {
+                val intentPreferences = Intent(this, PreferencesActivity::class.java)
+                startActivity(intentPreferences)
+                return true
+            }
+            R.id.acerca -> {
+                val intentPreferences = Intent(this, AboutActivity::class.java)
+                startActivity(intentPreferences)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
